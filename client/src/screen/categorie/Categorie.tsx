4703340438ebc8f31b4325eab96d502a6cd875e5
Faure-Thomas <56997397+Faure-Thomas@users.noreply.github.com>
2021-03-03 10:52:26 +0100

    working on filters

--- client/src/screen/categorie/Categorie.tsx
+++ client/src/screen/categorie/Categorie.tsx
@@ -4,6 +4,7 @@ import styles from "./style.module.scss";
 import Logo from "../../component/Logo/Logo";
 import {Container} from "../../component/ui/Template/Container";
 import {ProductGroup} from "../../component";
+import Filter, {Types} from "../../component/Filter/Filter";
 
 export default function HomeScreen(): JSX.Element {
     const {} = styles;
@@ -13,6 +14,9 @@ export default function HomeScreen(): JSX.Element {
             <Container>
                 <ProductGroup />
             </Container>
+            <Filter label="CATEGORIE" type={Types.list} categories={["T-Shirt", "Short"]} />
+            <Filter label="PRIX" type={Types.slice} categories={["T-Shirt", "Short"]} />
+            <Filter label="TAILLE" type={Types.checkbox} categories={["T-Shirt", "Short"]} />
         </div>
     );
 }
