774d89efa6705923a4186cc596cf79d2fb939ae4
RandyLarzabal <71065214+RandyLarzabal@users.noreply.github.com>
2021-03-03 10:46:08 +0100

    update

--- client/src/component/NavBar/NavBar.tsx
+++ client/src/component/NavBar/NavBar.tsx
@@ -5,54 +5,93 @@ import { Button } from "../index";
 import { IconList } from "../Icon/Icon";
 
 type Props = {
+    location: string;
     visibility: boolean;
     setVisibility: Dispatch<SetStateAction<boolean>>;
 };
 
 const NavBar: FunctionComponent<Props> = (props: Props) => {
-    const { visibility, setVisibility } = props;
-    const { navBar, close } = style;
-    return (
-        <div className={navBar} style={{ height: visibility ? "100vh" : 0 }}>
-            <Button
-                label={""}
-                className={close}
-                handleClick={() => setVisibility(false)}
-                icon={{ icon: IconList.shop, size: 40, color: "#000000" }}
-            />
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink to="/">Accueil</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/homme">Homme</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/femme">Femme</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/Streetwear">Streetwear</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/">Accueil</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/homme">Homme</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/femme">Femme</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/Streetwear">Streetwear</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/Streetwear">Streetwear</NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    );
+    const { visibility, setVisibility, location } = props;
+    const { navBar, close , menu } = style;
+
+    switch (location) {
+        case "/":
+            return (
+                <div className={navBar} style={{ height: visibility ? "100vh" : 0 }}>
+                    <Button
+                        label={""}
+                        className={close}
+                        handleClick={() => setVisibility(false)}
+                        icon={{ icon: IconList.shop, size: 40, color: "#000000" }}
+                    />
+                    <nav>
+                        <ul>
+                            <li>
+                                <NavLink to="/">Accueil</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/homme">Homme</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/femme">Femme</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/Streetwear">Streetwear</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/">Accueil</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/homme">Homme</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/femme">Femme</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/Streetwear">Streetwear</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/Streetwear">Streetwear</NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            );
+        default:
+            return (
+                <nav>
+                    <ul className={menu}>
+                        <li>
+                            <NavLink to="/">Accueil</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/homme">Homme</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/femme">Femme</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/Streetwear">Streetwear</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/">Accueil</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/homme">Homme</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/femme">Femme</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/Streetwear">Streetwear</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/Streetwear">Streetwear</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            );
+    }
 };
 
 export default NavBar;
--- client/src/screen/home/HomeScreen.tsx
+++ client/src/screen/home/HomeScreen.tsx
@@ -1,44 +1,21 @@
 import React, { useState } from "react";
 import { Button, NavBar, Title, Image } from "../../component";
 import global from "../../App.module.scss";
-import imghero from "../../assets/img/hero.jpg";
+
 import styles from "./style.module.scss";
 
-import ReassuranceCard from "../../component/ReassuranceGroup/ReassuranceCard/ReassuranceCard";
-import Logo from "../../component/Logo/Logo";
 
 import ProductGroup, { GroupType } from "../../component/ProductGroup/ProductGroup";
-import { IconList } from "../../component/Icon/Icon";
+
 import ReassuranceGroup from "../../component/ReassuranceGroup/ReassuranceGroup";
 
 export default function HomeScreen(): JSX.Element {
     const { container } = global;
     const { hero, button, navBar, heroImg, logoImg, greyBg } = styles;
 
-    const [visibleNavBack, setVisibleNavBack] = useState(false);
+
     return (
         <div>
-            <NavBar setVisibility={setVisibleNavBack} visibility={visibleNavBack} />
-            <div className={hero}>
-                <div className={navBar}>
-                    <img src="" alt="" />
-                    <Button
-                        handleClick={() => setVisibleNavBack(true)}
-                        color={""}
-                        label={""}
-                        icon={{ icon: IconList.menu, color: "#FFF", size: 40 }}
-                        className={button}
-                    />
-                </div>
-                <Logo />
-                <Image className={heroImg} src={imghero} alt={"Hero image"} size={"cover"} />
-                <div className={container}>
-                    <h1>LOREM IPSUM</h1>
-                    <p>LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT.</p>
-                    <Button label={"EN SAVOIR PLUS >>"} color={"#F58C03"} />
-                </div>
-            </div>
-
             <div className={container}>
                 <Title text={"Nos Nouveautés"} />
                 <ProductGroup groupType={GroupType.basic} />
